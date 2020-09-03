b6baa735be99624e4b1fca099eb33f244f89b5bf
obitrim <pevangelist516@gmail.com>
2020-09-03 17:45:49 +0000

    Updates

---
+++ precache-manifest.0680195e8f3ef56a914f3f56e322270c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3eee479ea436e01ff28de55a59ba9faf",
+    "url": "/todoist-front-end/index.html"
+  },
+  {
+    "revision": "97b88f1ce9aca9abefa7",
+    "url": "/todoist-front-end/static/css/main.8c8e13e0.chunk.css"
+  },
+  {
+    "revision": "138c0184ac62c6282dc0",
+    "url": "/todoist-front-end/static/js/2.a0fcb12d.chunk.js"
+  },
+  {
+    "revision": "77e7f328dacc981fca681ee4ae94d669",
+    "url": "/todoist-front-end/static/js/2.a0fcb12d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97b88f1ce9aca9abefa7",
+    "url": "/todoist-front-end/static/js/main.440d7632.chunk.js"
+  },
+  {
+    "revision": "b1d9560f945edef0faff",
+    "url": "/todoist-front-end/static/js/runtime-main.ad4946db.js"
+  }
+]);
